e118d187e0b3a6d8364507b277d6d45b9e336113
IIRobotNinjaII <antonythoppil@hotmail.com>
2023-12-10 12:30:08 +0530

    Mote C File and CORS Fix

--- rest-api-go/web/app.go
+++ rest-api-go/web/app.go
@@ -23,13 +23,28 @@ type OrgSetup struct {
 // Serve starts http web server.
 func Serve(setup1 OrgSetup, setup2 OrgSetup,setup3 OrgSetup) {
 
-	http.HandleFunc("/query", setup1.Query)
-	http.HandleFunc("/invokeorg1", setup1.Invoke)
-	http.HandleFunc("/invokeorg2", setup2.Invoke)
-	http.HandleFunc("/invokeorg3", setup3.Invoke)
+		http.HandleFunc("/query", enableCors(setup1.Query))	
+		http.HandleFunc("/invokeorg1", enableCors(setup1.Invoke))
+		http.HandleFunc("/invokeorg2", enableCors(setup2.Invoke))
+		http.HandleFunc("/invokeorg3", enableCors(setup3.Invoke))
 	
 	fmt.Println("Listening (http://localhost:3000/)...")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		fmt.Println(err)
+	}
+}
+
+func enableCors(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+
+		if req.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		handler(w, req)
 	}
 }
--- server-udp/server.js
+++ server-udp/server.js
@@ -17,18 +17,29 @@ serverUDP.bind(portIPv6, hostIPv6);
 
 function processMessage(message, remote) {
   // console.log(remote.address)
-    if (!disableLogs)
-        console.log('[UDP - IPv6] ' + new Date().toISOString() + ' ' + remote.address + ' Port:' + remote.port + ' - ' + message);
-    let dataArray = message.toString().split('|')
-    let id = dataArray[0].split(':')[1];
-    let org = dataArray[2].split(':')[5]
-    let temp = parseInt(dataArray[3]);
-    let hum = parseInt(dataArray[4]);
-    let pm = parseInt(dataArray[5]);
-    let co2 = parseInt(dataArray[6]); 
-    let voc = parseInt(dataArray[7]); 
-    let thermostat_on = parseInt(dataArray[8]); 
-    let humidifier_on = parseInt(dataArray[9]); 
+  let dataArray = message.toString().split('|')
+  let id = dataArray[0].split(':')[1];
+  let org = dataArray[2].split(':')[5]
+  let temp = parseInt(dataArray[3]);
+  let hum = parseInt(dataArray[4]);
+  let pm = parseInt(dataArray[5]);
+  let co2 = parseInt(dataArray[6]); 
+  let voc = parseInt(dataArray[7]); 
+  let thermostat_on = parseInt(dataArray[8]); 
+  let humidifier_on = parseInt(dataArray[9]); 
+  if (!disableLogs){
+    console.log('[UDP - IPv6] ' + new Date().toISOString())
+    console.log('Incoming from ' + remote.address + ' Port:' + remote.port);
+    console.log('Sensor ID : '+ id);
+    console.log('Belongs to Organization : '+org);
+    console.log('Temperature : '+temp); 
+    console.log('Humidity : '+hum);
+    console.log('CO2 : '+co2);
+    console.log('VOC : '+voc);
+    console.log('PM : '+pm);
+    console.log('Thermostat status : '+thermostat_on);
+    console.log('Humiifier status : '+humidifier_on+'\n');
+  }
     sendData(org,id,temp,hum,pm,co2,voc,thermostat_on,humidifier_on);
 }
 function sendData(org,id,temp,hum,pm,co2,voc,thermostat_on,humidifier_on) {
@@ -54,7 +65,7 @@ function sendData(org,id,temp,hum,pm,co2,voc,thermostat_on,humidifier_on) {
     },
   })
     .then(response => {
-      console.log('Response:', response.data);
+      console.log('Successfully added to the Blockchain : ', response.data);
     })
     .catch(error => {
       console.error('Error:', error);
